87201278b2d8a9c1a9d9e5a8075027867fb2619a
Taylor Smith <tbsmith0205@gmail.com>
2021-06-16 21:50:56 -0600

    completed paths for view.js in routes directory

--- routes/view.js
+++ routes/view.js
@@ -4,3 +4,11 @@ const router = require("express").Router();
 router.get("/exercise", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
