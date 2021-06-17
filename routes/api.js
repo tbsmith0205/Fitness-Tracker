3ed5a1978748cb8fea9c275b528cb030d7a461f5
Taylor Smith <tbsmith0205@gmail.com>
2021-06-16 21:30:38 -0600

    added functionality to add an exercise in api.js in routes directory

--- routes/api.js
+++ routes/api.js
@@ -12,3 +12,21 @@ router.post("/api/workouts", (req, res) => {
       console.log(err);
     });
 });
+
+// add an exercise
+router.put("api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(
+    req.params.id,
+    {
+      $push: { exercises: req.body },
+    },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
